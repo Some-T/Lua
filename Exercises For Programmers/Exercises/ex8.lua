b5e5f4897bb54319f4ea9947cccf455785c953da
Some-T <jamie.cropley@gmail.com>
2017-08-12 21:01:35 +0100

    Update ex8
    Update ex8

--- Exercises For Programmers/Exercises/ex8.lua
+++ Exercises For Programmers/Exercises/ex8.lua
@@ -9,5 +9,7 @@ totalSlices = pizzasAmount * slicesAmount
 io.write ("There are ", totalSlices, " slices/pieces of pizza in total. \n")
 slicesShared = math.floor(totalSlices / peopleAmount)
 io.write ("Each person gets ", (slicesShared), " slices/pieces of pizza. \n")
-io.write ("There are ", totalSlices - (slicesShared * peopleAmount), " slices/pieces of pizza left. \n")
-io.write(totalSlices % slicesShared)
+slicesLeft = totalSlices % peopleAmount
+io.write ("There are ", slicesLeft, " slices/pieces of pizza left. \n")
+--	Without using modulo:
+--	io.write ("There are ", totalSlices - (slicesShared * peopleAmount), " slices/pieces of pizza left. \n")
