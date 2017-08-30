7726a10aca79bfa0cf4ba4f237ddce3af22e81a2
Jamie Cropley <jamie.cropley@gmail.com>
2017-08-30 19:50:37 +0100

    Update
    Update

--- Games/Snake/conf.lua
+++ Games/Snake/conf.lua
@@ -1,1 +1,5 @@
-t.console=true
+io.stdout:setvbuf("no")
+
+function love.conf(t)            -- The LÖVE version this game was made for (string)
+    t.console = true
+end
--- Games/Snake/main.lua
+++ Games/Snake/main.lua
@@ -1,6 +1,12 @@
+io.stdout:setvbuf("no")
+
 -- Based off of the tutorial located at: https://simplegametutorials.github.io/snake/
 -- Please contact jamie.cropley@gmail.com for any matters relating to this game.
 
+io.write("testing")
+
+
+require = "/conf.lua"
 
 function love.draw()
     local gridXCount = 20
