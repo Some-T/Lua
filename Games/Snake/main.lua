e6ea96bb679ab927a74462d15c42b4272864cfa4
Jamie Cropley <jamie.cropley@gmail.com>
2017-08-30 18:13:15 +0100

    Update
    Update

---
+++ Games/Snake/main.lua
@@ -0,0 +1,18 @@
+-- Based off of the tutorial located at: https://simplegametutorials.github.io/snake/
+-- Please contact jamie.cropley@gmail.com for any matters relating to this game.
+
+
+function love.draw()
+    local gridXCount = 20
+    local gridYCount = 15
+    local cellSize = 15
+
+    love.graphics.setColor(71, 71, 71)
+    love.graphics.rectangle(
+        'fill',
+        0,
+        0,
+        gridXCount * cellSize,
+        gridYCount * cellSize
+    )
+end
