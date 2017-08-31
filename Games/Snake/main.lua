c69e87cb9f4746ea9a0db29fdfecb009538d63df
Jamie Cropley <jamie.cropley@gmail.com>
2017-08-31 20:28:11 +0100

    Update
    Update

--- Games/Snake/main.lua
+++ Games/Snake/main.lua
@@ -1,4 +1,4 @@
 -- Based off of the tutorial located at: https://simplegametutorials.github.io/snake/
 -- Please contact jamie.cropley@gmail.com for any matters relating to this game.
 
-io.write("Console output test \n")
+io.write("Console output test \n \n")
