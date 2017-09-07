526320832f5a51ba886e2387998fdb875d42323d
Jamie Cropley <jamie.cropley@gmail.com>
2017-09-07 19:39:12 +0100

    Update
    Update

---
+++ Exercises For Programmers/Exercises/ex11.lua
@@ -0,0 +1,10 @@
+io.write "How many euros are you exchanging? "
+amountFrom = io.read()
+io.write "What is the exchange rate? "
+rateFrom = io.read()
+
+
+-- io.write (amountFrom * rateFrom)
+
+-- 1 euro to usd = 1.20039
+-- 1 usd to euro = 0.833078
